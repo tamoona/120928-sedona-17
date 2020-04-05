56f09c167455a6efe6c83e4a0e7bd0a8f6cac263
Anastasiia <nanitamoonaboo@gmail.com>
2020-04-05 17:10:44 +0300

    Add task to deploy project to gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -15,28 +15,36 @@ var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
+var ghPages = require("gulp-gh-pages");
 
-gulp.task("server", function() {
+gulp.task("push", function () {
+  return gulp.src("./build/**/*").pipe(ghPages());
+});
+
+gulp.task("server", function () {
   server.init({
     server: "build/",
     notify: false,
     open: true,
     cors: true,
-    ui: false
+    ui: false,
   });
 
   gulp.watch("source/less/**/*.less", gulp.series("css", "refresh"));
   gulp.watch("source/js/**/*.js", gulp.series("copy", "refresh"));
-  gulp.watch("source/img/{icon-*,logo-htmlacademy}.svg", gulp.series("sprite", "html"));
+  gulp.watch(
+    "source/img/{icon-*,logo-htmlacademy}.svg",
+    gulp.series("sprite", "html")
+  );
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
-gulp.task("refresh", function(done) {
+gulp.task("refresh", function (done) {
   server.reload();
   done();
 });
 
-gulp.task("css", function() {
+gulp.task("css", function () {
   return gulp
     .src("source/less/style.less")
     .pipe(plumber())
@@ -50,7 +58,7 @@ gulp.task("css", function() {
     .pipe(gulp.dest("build/css"));
 });
 
-gulp.task("images", function() {
+gulp.task("images", function () {
   return gulp
     .src("source/img/**/*.{png,jpg,svg}")
     .pipe(
@@ -58,58 +66,58 @@ gulp.task("images", function() {
         imagemin.optipng({ optimizationLevel: 3 }),
         imagemin.jpegtran({ progressive: true }),
         imagemin.svgo({
-          plugins: [{ removeViewBox: false }]
-        })
+          plugins: [{ removeViewBox: false }],
+        }),
       ])
     )
     .pipe(gulp.dest("build/img"));
 });
 
-gulp.task("webp", function() {
+gulp.task("webp", function () {
   return gulp
     .src("source/img/**/*.{png,jpg}")
     .pipe(webp({ quality: 75 }))
     .pipe(gulp.dest("build/img"));
 });
 
-gulp.task("sprite", function() {
+gulp.task("sprite", function () {
   return gulp
     .src("source/img/{icon-*,logo-htmlacademy}.svg")
     .pipe(
       imagemin([
         imagemin.svgo({
-          plugins: [{ removeViewBox: false }]
-        })
+          plugins: [{ removeViewBox: false }],
+        }),
       ])
     )
     .pipe(
       svgstore({
-        inlineSvg: true
+        inlineSvg: true,
       })
     )
     .pipe(rename("sprite.svg"))
     .pipe(gulp.dest("build/img"));
 });
 
-gulp.task("html", function() {
+gulp.task("html", function () {
   return gulp
     .src("source/*.html")
     .pipe(posthtml([include()]))
     .pipe(gulp.dest("build"));
 });
 
-gulp.task("copy", function() {
+gulp.task("copy", function () {
   return gulp
     .src(["source/fonts/**/*.{woff,woff2}", "source/img/**", "source/js/**"], {
-      base: "source"
+      base: "source",
     })
     .pipe(gulp.dest("build"));
 });
 
-gulp.task("clean", function() {
+gulp.task("clean", function () {
   return del("build");
 });
 
 gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
-
 gulp.task("start", gulp.series("build", "server"));
+gulp.task("deploy", gulp.series("build", "push"));
