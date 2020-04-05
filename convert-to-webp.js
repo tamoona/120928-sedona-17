e8538aba60cfe53eecfd501a3fe083a5af29be11
Anastasiia <nanitamoonaboo@gmail.com>
2020-04-05 17:07:36 +0300

    Update 2020-04-05T14:07:28.346Z

--- convert-to-webp.js
+++
@@ -1,7 +0,0 @@
-const imagemin = require("imagemin");
-const webp = require("imagemin-webp");
-
-imagemin(["./source/img/*.{jpg,png}"], {
-  destination: "./source/img",
-  plugins: [webp({ quality: 75 })]
-});
